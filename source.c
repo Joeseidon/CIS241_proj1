7fd7826269cf547de3f7414c9abbc819c1d25baa
Joseph <cutinoj@mail.gvsu.edu>
2018-02-01 20:46:22 -0500

    comments

--- source.c
+++ source.c
@@ -1,55 +1,66 @@
+/*****************************************************
+*!Project: CIS241 Project 1
+*!
+*!Author: Joseph Cutino
+*!Description: This program using command line arguments 
+*!	to encode or decode text files. The user will
+*!	provide an input file, output file, crypto key,
+*!	and their desired action (encode (e) or decode (d))
+*!	as command line arguments.
+******************************************************/
+
+/*Library Includes*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
+/*Program Defines*/
 #define CHAR_COUNT 26
 
-
+/*Global Data*/
 typedef enum{
 	ENCODE = 0,
 	DECODE,
 	INVALID
 }op_mode;
 
+/*Function Prototypes*/
 void generateCharMap(char *key, char *alpha);
 void decode(FILE *in, FILE *out, char *lookupTbl);
 void encode(FILE *in, FILE *out, char *lookupTbl);
 
-/*DEBUGGING FUNCTIONS*/
-void printCharMap(char *map);
-void printFile(FILE *in);
-
 int main(int argc, char *argv[])   
 {
+	/*Variable Definition*/
 	op_mode mode = INVALID;
 	FILE *input, *output;
 	char *input_file_name, *output_file_name;
 	char *key;
 	char charMap[26];
-#ifndef DEBUG
-	//If more than one command line argument is present, quit
+	
+	/*Input Error Check*/
 	if(argc<5){
+		//This program requires 5 arguments to run properly
 		fprintf(stdout,"This program requires 5 arguments.\n");
-		exit(0);
+		exit(1);
 	}else{
 		//assign command line args to usable vars
 		input_file_name = argv[1];
 		output_file_name = argv[3];
 		key = argv[2];
 		if(argv[4][0] == 'e'){
-			//encryption mode 
+			//Encryption mode 
 			mode = ENCODE;
 		}else if (argv[4][0] == 'd'){
-			//decryption mode
+			//Decryption mode
 			mode = DECODE;
 		}else{
+			//Invalid operation
 			mode = INVALID;
 			exit(1);
 		}
 	}
-
-	
 	
 	//Open input and output files
 	input = fopen (input_file_name,"r" );
@@ -68,65 +79,18 @@ int main(int argc, char *argv[])
 		decode(input,output,charMap);
 	}else if(mode == DECODE){
 		encode(input,output,charMap);
-	}
-#else
-	FILE *encrypted;
-	FILE *decrypted;
-	generateCharMap("RaNdKey", charMap);
-	printCharMap(charMap);
-
-	input = fopen ("input.txt","r" );
-	output = fopen ("encrypted.txt", "w" );
-	
-	encode(input,output,charMap);
-	
-	fclose(output);
-	fclose(input);
-	
-	input = fopen ("input.txt","r" );
-	printf("\n\n/***********************ORIGINAL***********************/\n");
-	printFile(input);
-	fclose(input);
-	
-	encrypted = fopen("encrypted.txt","r");
-	printf("\n\n/***********************ENCRYPTED***********************/\n");
-	printFile(encrypted);
-	fclose(encrypted);
-	encrypted = fopen("encrypted.txt","r");
-	decrypted = fopen("decrypted.txt","w");
-	decode(encrypted,decrypted,charMap);
-	fclose(encrypted);
-	fclose(decrypted);
-	decrypted = fopen("decrypted.txt","r");
-	printf("\n\n/***********************DECRYPTED***********************/\n");
-	printFile(decrypted);
-	fclose(decrypted);
-	printf("\n\n\n");
-#endif
-	
+	}	
 	return 0;
 }
-void printFile(FILE *in){
-	char input;
-	fscanf(in, "%c", &input);
-	//If not end of file process chars
-	while ( ! feof(in) )   {
-		printf("%c",input);
-		//Read next character
-		fscanf ( in, "%c", &input );
-	}
-}
-void printCharMap(char *map){
-	int i;
-	for(i=0; i< CHAR_COUNT; i++){
-		printf("%c | ",(char)(0x61+i));
-	}
-	printf("\n");
-	for(i=0; i<CHAR_COUNT; i++){
-		printf("%c | ",map[i]);
-	}
-	printf("\n");
-}
+
+/*****************************************************
+*!Input: Input file ID, Output file ID, look up Table
+*!
+*!Output: NONE
+*!
+*!Description: Decode the text in the input file and 
+*!	print it to the output file.
+*****************************************************/
 void decode(FILE *in, FILE *out, char *lookupTbl){
 	char input;
 	int upper = 0;
@@ -162,6 +126,15 @@ void decode(FILE *in, FILE *out, char *lookupTbl){
 	}
 	return;
 }
+
+/*****************************************************
+*!Input: Input file ID, Output file ID, look up Table
+*!
+*!Output: NONE
+*!
+*!Description: Encode the text in the input file and 
+*!	print it to the output file.
+*****************************************************/
 void encode(FILE *in, FILE *out, char *lookupTbl){
 	char input;
 	int upper = 0;
@@ -191,18 +164,23 @@ void encode(FILE *in, FILE *out, char *lookupTbl){
 	}
 	return;
 }
-//using the key string and the concepts behind a ceaser cipher and new alphabet
-//will be generated.
+
+/**************************************************
+*!Input: Key and storage area for the look up Table
+*!
+*!Output: Look up table will be store in alpha
+*!
+*!Description: Using the key, an encryption
+*!	and decryption lookup table is created.
+**************************************************/
 void generateCharMap(char *key, char *alpha){
 	//for char in string add to alpha
-	//then starting with int value for 'a' if not int key? add to array
-
 	size_t len = strlen(key);
 	int i;
 	for(i = 0; i<len; i++){
 		alpha[i]=tolower(key[i]); // make sure lower case 
 	}
-
+	//Add the remaining characters to the array
 	char *rtn;
 	int missed =0;
 	for(i=0; i<CHAR_COUNT; i++){
